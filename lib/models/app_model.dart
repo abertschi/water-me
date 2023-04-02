598b04aaf2b60c56c69731f615c704128dfcb8e1
hi@abertschi.ch <hi@abertschi.ch>
2023-04-02 13:49:50 +0200

    update model to provide import/export

--- lib/models/app_model.dart
+++ lib/models/app_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:water_me/models/plant_model.dart';
 
--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:external_path/external_path.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:path_provider/path_provider.dart';
 import '../models/app_model.dart';
 
 class Db {
@@ -10,6 +12,26 @@ class Db {
   get model async {
     _model ??= await loadModel();
     return _model;
+  }
+
+  Future<String> get exportPath async {
+    final directory = await getExternalStorageDirectory();
+    return "${directory!.path}/water_me.json";
+  }
+
+  Future<String> get exportBackupPath async {
+    final directory = await getExternalStorageDirectory();
+    return "${directory!.path}/water_me.backup.json";
+  }
+
+  Future<File> get exportFile async {
+    final path = await exportPath;
+    return File(path!);
+  }
+
+  Future<File> get exportBackupFile async {
+    final path = await exportBackupPath;
+    return File(path!);
   }
 
   void saveModel(AppModel model) async {
@@ -33,5 +55,22 @@ class Db {
     } else {
       return AppModel.fromJson(json.decode(m));
     }
+  }
+
+  // throws exception on unsuccess
+  Future<File> exportToFile(AppModel model, File file) async {
+    return file.writeAsString(await exportAsJsonString(model));
+  }
+
+  Future<String> exportAsJsonString(AppModel model) async {
+    return json.encode(model.toJson());
+  }
+
+  AppModel importJsonString(String jsonStr) {
+    // XXX: Throws exception on issue
+    AppModel model = AppModel.fromJson(json.decode(jsonStr));
+    _model = model;
+    saveModel(model);
+    return _model!;
   }
 }
